deb57b4d0b6badb72dd4cb884fbf18bfa865184d
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-07-09 17:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const button = document.getElementById("btn");
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("clicked");
+
+  // Get form inputs
+  const nameInput = document.getElementById("name");
+  const ageInput = document.getElementById("age");
+
+  // Get values from inputs
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value;
+
+  // Validate inputs
+  if (!age || !name) {
+    alert("inputs cannot be empty");
+    return;
+  }
+  // Create a promise for form submission handling
+  const submissionPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  submissionPromise
+    .then((message) => {
+      alert(message);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
